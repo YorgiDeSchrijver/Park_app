9ca02d7f07a3f2d3b2c63cc223d9745d29a24afb
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2023-05-18 11:30:55 +0200

    Removing logic from widget to controller

--- lib/src/features/core/controllers/navigation_controller.dart
+++ lib/src/features/core/controllers/navigation_controller.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:location/location.dart';
 import 'package:project/src/features/core/models/parking_spot_model.dart';
 import 'package:project/src/repository/navigation_repository/navigation_repository.dart';
-import 'package:project/src/repository/user_repository/user_repository.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
 
 class NavigationController extends GetxController {
-
   final RxList<ParkingSpotModel> parkingSpots = RxList<ParkingSpotModel>([]);
+  final Rx<MapLatLng> selectedMarkerLatLng = Rx<MapLatLng>(MapLatLng(51, 4.5));
+  final zoomPanBehavior = MapZoomPanBehavior().obs;
+  final mapController = MapTileLayerController();
+  final selectedMarkerIndex = RxInt(0);
 
   final navigationRepo = Get.put(NavigationRepository());
 
@@ -16,6 +16,13 @@ class NavigationController extends GetxController {
   void onInit() {
     super.onInit();
     loadParkingSpots();
+    zoomPanBehavior.value = MapZoomPanBehavior(
+      focalLatLng: MapLatLng(51, 4.5),
+      minZoomLevel: 3,
+      maxZoomLevel: 20,
+      zoomLevel: 14,
+      enableDoubleTapZooming: true,
+    );
   }
 
   Future<void> loadParkingSpots() async {
@@ -24,5 +31,19 @@ class NavigationController extends GetxController {
 
   Future<List<ParkingSpotModel>> getParkingSpots() async {
     return await navigationRepo.getAllParkingSpots();
+  }
+
+  Future<void> setFocalLatLng(MapLatLng focalLatLng) async {
+    zoomPanBehavior.value.focalLatLng = focalLatLng;
+  }
+
+  Future<void> selectMarker(int index) async {
+    selectedMarkerIndex.value = index;
+    final locations = parkingSpots.value;
+    final focalLatLng = MapLatLng(
+      locations[selectedMarkerIndex.value].latitude,
+      locations[selectedMarkerIndex.value].longitude,
+    );
+    setFocalLatLng(focalLatLng);
   }
 }
--- lib/src/features/core/screens/Navigation/widgets/navigation_map_widget.dart
+++ lib/src/features/core/screens/Navigation/widgets/navigation_map_widget.dart
@@ -15,7 +15,7 @@ class NavigationMapWidget extends StatefulWidget {
 }
 
 class _NavigationMapWidgetState extends State<NavigationMapWidget> {
-  late MapTileLayerController mapController;
+  MapTileLayerController? mapController;
   int? selectedMarkerIndex; // Variable to track the selected marker index
 
   @override
@@ -28,73 +28,65 @@ class _NavigationMapWidgetState extends State<NavigationMapWidget> {
   Widget build(BuildContext context) {
     final navigationController = Get.put(NavigationController());
 
-    return Obx(() {
-      final List<ParkingSpotModel> locations = navigationController.parkingSpots;
-      if (locations.isNotEmpty) {
-        return SfMaps(
-          layers: [
-            MapTileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              zoomPanBehavior: MapZoomPanBehavior(
-                minZoomLevel: 3,
-                maxZoomLevel: 20,
-                focalLatLng: MapLatLng(
-                  locations[selectedMarkerIndex ?? 0].latitude,
-                  locations[selectedMarkerIndex ?? 0].longitude,
+    return Obx(
+      () {
+        final List<ParkingSpotModel> locations =
+            navigationController.parkingSpots;
+        if (locations.isNotEmpty) {
+          return SfMaps(
+            layers: [
+              MapTileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                zoomPanBehavior: navigationController.zoomPanBehavior.value,
+                controller: mapController,
+                initialMarkersCount: locations.length,
+                tooltipSettings: const MapTooltipSettings(
+                  color: Colors.transparent,
                 ),
-                enableDoubleTapZooming: true,
-              ),
-              controller: mapController,
-              initialMarkersCount: locations.length,
-              tooltipSettings: const MapTooltipSettings(
-                color: Colors.transparent,
-              ),
-              // MARKER BUILDER
-              markerBuilder: (BuildContext context, int index) {
-                final bool isSelected = selectedMarkerIndex == index;
-                final double markerSize = isSelected ? 40 : 25;
+                markerBuilder: (BuildContext context, int index) {
+                  final bool isSelected = selectedMarkerIndex == index;
+                  final double markerSize = isSelected ? 40 : 25;
 
-                return MapMarker(
-                  latitude: locations[index].latitude,
-                  longitude: locations[index].longitude,
-                  alignment: Alignment.bottomCenter,
-                  child: InkWell(
-                    onTap: () {
-                      setState(() {
-                        selectedMarkerIndex = index; // Update the selected marker index
-                      });
+                  return MapMarker(
+                    latitude: locations[index].latitude,
+                    longitude: locations[index].longitude,
+                    alignment: Alignment.bottomCenter,
+                    size: Size(markerSize, markerSize),
+                    child: InkWell(
+                      onTap: () {
+                        navigationController.selectMarker(index);
 
-                      // Show the bottom sheet here
-                      showModalBottomSheet(
-                        context: context,
-                        builder: (context) {
-                          return Container(
-                            // Add your desired content for the bottom sheet
-                          );
-                        },
-                      );
-                    },
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 250),
-                      height: markerSize,
-                      width: markerSize,
-                      child: FittedBox(
-                        child: Icon(
-                          Icons.location_on,
-                          color: isSelected ? Colors.blue : Colors.red,
-                          size: 10,
+                        // Show the bottom sheet here
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return Container(
+                                // Add your desired content for the bottom sheet
+                                );
+                          },
+                        );
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 250),
+                        height: markerSize,
+                        width: markerSize,
+                        child: FittedBox(
+                          child: Icon(
+                            Icons.location_on,
+                            color: isSelected ? Colors.blue : Colors.red,
+                            size: 10,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  size: Size(markerSize, markerSize),
-                );
-              },
-            ),
-          ],
-        );
-      }
-      return Center(child: CircularProgressIndicator());
-    });
+                  );
+                },
+              ),
+            ],
+          );
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+    );
   }
 }
